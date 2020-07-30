8381895305cbc84e70a3e1afbcdb1eb4ab53a430
Audry Wolters <audrywolters@gmail.com>
2020-07-30 02:41:52 -0500

    base mode working. secrets not showing.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
 
-    pool.query('SELECT * FROM "secret";')
+    let queryString = 'SELECT * FROM "secret" WHERE "secrecy_level" < $1;';
+    
+    pool.query( queryString, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
